534f03e6ffbe8b0d3d98bb62db99f200a3486668
Emmanuel Tadena Jr <emmanueltadenajr@gmail.com>
2020-12-10 11:41:34 +0800

    Modified AddNote and Note file

--- todo-app/app/screens/AddNote.js
+++ todo-app/app/screens/AddNote.js
@@ -1,12 +1,23 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { TextInput, FAB } from "react-native-paper";
 
 function AddNote() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Add Note</Text>
-    </View>
+    <React.Fragment>
+      <View style={styles.container}>
+        <TextInput label="Title Notes" mode="outlined" style={styles.title} />
+        <TextInput
+          label="Notes"
+          mode="outlined"
+          multiline={true}
+          scrollEnabled={true}
+          returnKeyType="done"
+          style={styles.textInput}
+        />
+      </View>
+      <FAB style={styles.fab} small icon="check" />
+    </React.Fragment>
   );
 }
 
@@ -16,7 +27,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  title: {
+    fontSize: 20,
+    width: "75%",
+    marginBottom: 15,
+  },
+  textInput: {
+    fontSize: 16,
+    height: 200,
+    width: "75%",
+  },
   text: {
     fontSize: 24,
   },
+  fab: {
+    backgroundColor: "gray",
+    position: "absolute",
+    margin: 15,
+    right: 0,
+    bottom: 0,
+  },
 });
+
+export default AddNote;
--- todo-app/app/screens/Notes.js
+++ todo-app/app/screens/Notes.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, FAB } from "react-native-paper";
 
-function Notes() {
+function Notes({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={style.text}>View Notes</Text>
+      <Text style={styles.text}>View Notes</Text>
+      <FAB
+        style={styles.fab}
+        label="Add Notes"
+        small
+        onPress={() => navigation.navigate("AddNote")}
+      />
     </View>
   );
 }
@@ -15,6 +21,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  fab: {
+    backgroundColor: "gray",
+    position: "absolute",
+    margin: 20,
+    right: 0,
+    bottom: 0,
   },
 });
 
