a8c8e2b4e5ef7491b7a2c507441f81098fbb8a10
Emmanuel Tadena Jr <emmanueltadenajr@gmail.com>
2020-12-20 14:48:29 +0800

    Add AppButton components

---
+++ todo-app-v01/app/components/AppButton.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+
+export default function ({ children, onPress, styleView, styleText }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={[styles.containerView, styleView]}>
+        <Text style={[styles.containerText, styleText]}>{children}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerText: {
+    fontFamily: "sans-serif-thin",
+    fontSize: 18,
+  },
+});
