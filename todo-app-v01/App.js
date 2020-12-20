0b9d7dbd68972becb1b05c625c009c40546955a7
Emmanuel Tadena Jr <emmanueltadenajr@gmail.com>
2020-12-20 12:58:54 +0800

    Modified the App.js file adding Navigation capabilities

--- todo-app-v01/App.js
+++ todo-app-v01/App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Notes from "./app/screens/Notes";
+import AddNotes from "./app/screens/AddNotes";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Notes">
+        <Stack.Screen name="Notes" component={Notes} />
+        <Stack.Screen name="AddNotes" component={AddNotes} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
